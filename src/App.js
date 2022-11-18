416f54405e0efb562117486211f64818d91a4295
Daniel Schmidt <daniel.schmidt@tuni.fi>
2022-11-18 08:21:09 +0200

    init repo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import ItalyPage from "./pages/Italy";
+import BrazilPage from "./pages/Brazil";
+import FinlandPage from "./pages/Finland";
+
+const App = () => {
+  return (
+    <div>
+      <FinlandPage />
+      <ItalyPage />
+      <BrazilPage />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/Finland.js
@@ -0,0 +1,9 @@
+const FinlandPage = () => {
+  return (
+    <div>
+      <h2>You are at Helsinki airport. Where do you want to travel?</h2>
+    </div>
+  );
+};
+
+export default FinlandPage;
